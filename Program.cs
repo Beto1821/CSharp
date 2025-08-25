9d9f7d87bcca3f0ca9103db28ab23c95d2cf86f0
Adalberto R. Ribeiro <beto1821@uol.com.br>
2025-08-24 23:16:22 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using ExDOTNET.Models;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Pessoa p = new Pessoa();
+        p.Nome = "Adalberto";
+        p.Idade = 50;
+        p.Apresentar();
+    }
+}
